9de73c29980ea7f988185440b94fc561ce1f8474
sowjanya <sowji.muppala@gmail.com>
2025-09-30 23:06:37 +0530

    add

---
+++ mysql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+user=$(id -u)
+if [ $? -ne 0 ]; then 
+    echo "please proceed with root user pprevilage"
+    exit 1
+fi
+script_dir=$PWD
+logs_folder="/var/log/expense-script"
+script_file= $( echo $0 | cut -d "." -f1 )
+mkdir -p $logs_folder
+log_file=$logs_folder/$script_file.log
+echo "$log_file"
