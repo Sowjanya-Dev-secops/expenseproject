b536cfafd0b5298c5425c23e1a282b6aefa821a8
sowjanya <sowji.muppala@gmail.com>
2025-09-30 23:11:50 +0530

    add

--- mysql.sh
+++ mysql.sh
@@ -7,7 +7,9 @@ if [ $user -ne 0 ]; then
 fi
 script_dir=$PWD
 logs_folder="/var/log/expense-script"
-script_file= $( echo $0 | cut -d "." -f1 )
+script_file=$( echo $0 | cut -d "." -f1 )
+
+
 mkdir -p $logs_folder
 log_file=$logs_folder/$script_file.log
 echo "$log_file"
