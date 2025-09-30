9f2f199226bc72038879669e34d68164ae872de9
sowjanya <sowji.muppala@gmail.com>
2025-10-01 00:10:50 +0530

    add

--- expensebackend.sh
+++ expensebackend.sh
@@ -24,37 +24,38 @@ validate(){
         exit 1
     else
         echo -e "$G success $N:: $2 is succes "
+    fi
 }
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>>log_file
 validate $? "disable nodejs"
-dnf module enable nodejs:20 -y
+dnf module enable nodejs:20 -y &>>log_file
 validate $? "enable nodejs"
-dnf install nodejs -y
+dnf install nodejs -y &>>log_file
 validate $? "install nodejs"
-useradd expense
-validate $? "user add"
+useradd expense &>>log_file
+validate $? "user add" 
 mkdir /app
 
-curl -o /tmp/backend.zip https://expense-joindevops.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+curl -o /tmp/backend.zip https://expense-joindevops.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>log_file
 validate $? "down load application code"
 cd /app
-unzip /tmp/backend.zip
+unzip /tmp/backend.zip &>>log_file
 validate $? "unzip code"
 npm install
-validate $? "npm install"
+validate $? "npm install" &>>log_file
 cp  $Scrpt_Dir/expense.service /etc/systemd/system/backend.service
 validate $? "copy system controll code"
-systemctl daemon-reload
-systemctl start backend
-validate $? "start backend"
-systemctl enable backend
-validate $? "enable backend"
-dnf install mysql -y
+systemctl daemon-reload 
+systemctl start backend &>>log_file
+validate $? "start backend" 
+systemctl enable backend &>>log_file
+validate $? "enable backend" 
+dnf install mysql -y &>>log_file
 validate $? "install mysql"
-mysql -h mysql.msdevsecops.fun -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h mysql.msdevsecops.fun -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>log_file
 validate $? "dns add"
-systemctl restart backend
+systemctl restart backend &>>log_file
 validate $? "restart backend"
 
 
